6b70d28fe31f0d968c8c92886c7fcf38eb2f8486
ubuntu <joaoantoniosittamartins@gmail.com>
2024-03-07 19:39:56 -0300

    Complete Review

--- Recursive_function.c
+++ Recursive_function.c
@@ -2,8 +2,28 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main(){
+/* 3 * 2 * 1 */
 
+int Factorial_Calculator(int number){
+    int fat;
+
+    if (number <= 1){
+        return 1;
+    } else {
+        fat = number * Factorial_Calculator(number -1);
+        return fat;
+    }
+}
+
+int main(){
+    int num, result;
+
+    printf("Type the number to calculate the factorial:\n");
+    scanf("%d", &num);
+    setbuf(stdin, NULL);
+
+    result = Factorial_Calculator(num);
+    printf("The factorial of %d is %d\n", num, result);
 
     return 0;
 }
---
+++ Structs.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+typedef struct {
+    char name[30];
+    float grade[4]; // 0-10
+} Alunos;
+
+
+void Print_Student(Alunos Aluno){
+
+    printf("Name: %s \n",Aluno.name);
+    printf("Grades:");
+    for (int i = 0; i < 4; i++){
+        printf("%.2f", Aluno.grade[i]);
+    }
+    printf("\n");
+        
+    
+
+    return;
+}
+
+
+int main(){
+    Alunos Aluno;
+
+    printf("Type the  name of the student:\n");
+    fgets(Aluno.name, sizeof(Aluno.name), stdin);
+    Aluno.name[strcspn(Aluno.name, "\n")] = 0;
+
+    for (int i = 0; i < 4; i++){
+    printf("Type the Grade[%i]: \n", i+1);
+    scanf("%f ", &Aluno.grade[i]);
+    setbuf(stdin ,NULL);
+    }
+
+    Print_Student(Aluno);
+    
+    
+    return 0;
+}
